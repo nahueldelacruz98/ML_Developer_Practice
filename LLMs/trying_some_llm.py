2daef7d7ec0ba10c3de8116d652d302df5a718b7
Nahuel De la cruz <nahulmz98@gmail.com>
2025-10-09 00:28:33 -0300

    Mistral llm model

--- LLMs/trying_some_llm.py
+++ LLMs/trying_some_llm.py
@@ -2,6 +2,7 @@
 
 from transformers import pipeline
 import torch
+from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 import os
 
@@ -10,11 +11,24 @@ load_dotenv()
 def openai_light_model():
     # Use a pipeline as a high-level helper
 
-    pipe = pipeline("text-generation", model="openai/gpt-oss-20b")
+    model_id = "openai/gpt-oss-120b"
+    pipe = pipeline(
+        "text-generation",
+        model=model_id,
+        torch_dtype="auto",
+        device_map="auto",
+    )
+
     messages = [
-        {"role": "user", "content": "Who are you?"},
+        {"role": "user", "content": "Explain quantum mechanics clearly and concisely."},
     ]
-    print(pipe(messages))
+
+    outputs = pipe(
+        messages,
+        max_new_tokens=256,
+    )
+    print(outputs[0]["generated_text"][-1])
+
 
 def ollama_model():
     
@@ -27,4 +41,27 @@ def ollama_model():
 
     print(pipe("Hey how are you doing today?"))
 
-ollama_model() 
+def mistral_model():
+    client = InferenceClient(
+        model="mistralai/Mistral-7B-Instruct-v0.2"
+        #token=os.environ.get("HF_ACCESS_TOKEN") # Safely loads token from environment
+    )
+
+    messages = [
+        {"role": "user", "content": "What are the three most important things to know about Mistral-7B-Instruct-v0.2?"}
+    ]
+
+    # Use chat.completions.create for conversational tasks
+    response = client.chat.completions.create(
+        model="mistralai/Mistral-7B-Instruct-v0.2", # May be needed again depending on your client setup
+        messages=messages,
+        max_tokens=256
+    )
+
+    # Extract the generated text
+    generated_text = response.choices[0].message.content
+    print(generated_text)
+
+#ollama_model() 
+#openai_light_model()
+mistral_model()  #continue exploring huggingface models: https://www.youtube.com/watch?v=RL1XxStSzgA
